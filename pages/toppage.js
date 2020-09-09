50e9801e6e0cd16a77e1fb872200216c0a0f2e40
TakuoIshida <archersparadox.100@gmail.com>
2020-09-09 22:49:49 +0900

    routhing

---
+++ pages/toppage.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import Link from 'next/link'
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>This is Toppage</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div>
+          <Link href="/readme" className="card">
+          <a>React.jsにNext.jsを追加するメリット</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="/readme" className="card">
+          <a>環境構築方法</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="/readme" className="card">
+          <a>Dev画面</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="/readme" className="card">
+          <a>read me!</a>
+          </Link>
+        </div>
+      </main>
+
+    </div>
+  )
+}
