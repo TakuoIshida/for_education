8a5187010ed7ed2eac079fa91a1778cc08c48fe8
takuo_ishida <archersparadox.100@gmail.com>
2020-10-24 09:41:16 +0900

    nprogress 変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,19 @@ import Head from 'next/head'
 import NProgress from 'nprogress'
 import Router from 'next/router'
 
-Router.events.on('routeChangeStart', () => NProgress.inc())
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
+NProgress.configure({ showSpinner: false });
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+});
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <Provider store={store}>
